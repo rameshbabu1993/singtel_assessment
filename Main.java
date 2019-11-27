3e0a8388ddbd23fed70aad2770d748814983c517
user <user@N2V-Mac.local>
2019-11-28 01:38:40 +0800

    fish class added

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ class Animal {
     void walk() {
         System.out.println("I am walking");
     }
+
+    void sing(){
+        System.out.println("I am Singing");
+    }
 }
 
 class Bird extends Animal {
@@ -18,13 +22,42 @@ class Bird extends Animal {
     void fly() {
         System.out.println("I am flying");
     }
-
-    void sing(){
-        System.out.println("I am Singing");
-    }
     
     void say(){
         System.out.println(this.getClass().getSimpleName() + " says " + this.voice);
+    }
+}
+
+
+class Fish extends Animal {
+
+    String did = "", appreance = "";
+
+    public Fish(){};
+
+    public Fish(String appreance, String did){
+        this.did = did;
+        this.appreance = appreance;
+    }
+
+    void walk() {
+        System.out.println(this.getClass().getSimpleName() + " do not walk");
+    }
+
+    void sing(){
+        System.out.println(this.getClass().getSimpleName() + " do not sing");
+    }
+    
+    void swim(){
+        System.out.println(this.getClass().getSimpleName() + " can swim");
+    }
+
+    void howItLook(){
+         System.out.println(this.getClass().getSimpleName() + " " + this.appreance);
+    }
+
+    void does(){
+         System.out.println(this.getClass().getSimpleName() + " " + this.did);
     }
 }
 
@@ -67,6 +100,30 @@ class Parrot extends Bird {
     }
 }
 
+class Shark extends Fish {
+
+    public Shark(String appreance, String did){
+        super(appreance, did);
+    }
+
+}
+
+class Clownfish extends Fish {
+
+    public Clownfish(String appreance, String did){
+        super(appreance, did);
+    }
+
+}
+
+class Dolphins extends Fish {
+
+    public Dolphins(String appreance, String did){
+        super(appreance, did);
+    }
+
+}
+
 
 public class Solution {
     public static void main(String[] args) {
@@ -100,5 +157,26 @@ public class Solution {
         // parrot with cat
         Parrot rooster_parrot = new Parrot("Cock-a-doodle-doo with a rooster"); 
         rooster_parrot.say();
+
+        // Fish
+        Fish fish = new Fish();
+        fish.sing();
+        fish.walk();
+        fish.swim();
+
+        // shark
+        Shark shark = new Shark("large and grey", "eat other fish");
+        shark.howItLook();
+        shark.does();
+
+        // clownfish
+        Clownfish clownfish = new Clownfish("are small and colourful (orange)", "make jokes");
+        clownfish.howItLook();
+        clownfish.does();
+
+        // dolfins
+        Dolphins dolphins = new Dolphins("are beautifull", "help others");
+        dolphins.howItLook();
+        dolphins.does();
     }
 }
