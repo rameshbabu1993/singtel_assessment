6390ca649e0d9e6ef797e2e1efb397eb967aa0b2
user <user@N2V-Mac.local>
2019-11-28 01:13:20 +0800

    Duck and Chicken class added

--- Main.java
+++ Main.java
@@ -1,10 +1,20 @@
 class Animal {
+
     void walk() {
         System.out.println("I am walking");
     }
 }
 
 class Bird extends Animal {
+
+    String voice = ""; 
+
+    public Bird(){};
+
+    public Bird(String voice){
+        this.voice = voice;
+    }
+
     void fly() {
         System.out.println("I am flying");
     }
@@ -14,11 +24,54 @@ class Bird extends Animal {
     }
 }
 
+
+
+class Duck extends Bird {
+
+    public Duck(String voice){
+        super(voice);
+    }
+
+    void say(){
+        System.out.println("Duck Says" + super.voice);   
+    }
+
+    void swim(){
+        System.out.println("I am swimming");
+    }
+}
+
+class Chicken extends Bird {
+
+    public Chicken(String voice){
+        super(voice);
+    }
+
+    void say(){
+        System.out.println("Chicken Says" + super.voice);   
+    }
+
+    void fly(){
+        System.out.println("I can't able to fly");
+    }
+}
+
+
 public class Solution {
     public static void main(String[] args) {
         Bird bird = new Bird();
         bird.walk();
         bird.fly();
         bird.sing();
+
+        // duck 
+        Duck duck = new Duck("Quack, quack");
+        duck.say();
+        duck.swim();
+
+        // chicken
+        Chicken chicken = new Chicken("Cluck, cluck");
+        chicken.say();
+        chicken.fly();
     }
 }
